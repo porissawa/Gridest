518b0a99a6219ff5e7d7d86ad33f41f2642b0b0a
porissawa <porissawa@users.noreply.github.com>
2019-01-29 15:42:59 -0200

    changed to vanilla JS. Adjusted grid for collage layout.

---
+++ main.js
@@ -0,0 +1,16 @@
+let imgGrid = document.getElementsByClassName('grid')
+let gridSize = document.getElementById('grid_size')
+let htmlStyles = window.getComputedStyle(document.querySelector("html"))
+let gridRows = parseInt(htmlStyles.getPropertyValue("--rowNum"))
+let gridCols = parseInt(htmlStyles.getPropertyValue("--colNum"))
+
+gridSize.addEventListener('change', adjustGrid)
+
+function adjustGrid() {
+    let gridValue = gridSize.value
+
+    document.documentElement.style.setProperty("--rowNum", gridValue)
+    document.documentElement.style.setProperty("--colNum", gridValue)
+
+    // console.log('Cols = ' + gridCols.value + ' Rows = ' + gridRows.value)
+}
